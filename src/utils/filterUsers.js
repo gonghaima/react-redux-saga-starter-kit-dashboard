4b66c34118c1637de6be746a46d9a402d465313d
Steven <gonghaima@hotmail.com>
2019-03-25 09:42:20 +1100

    destructure for config

--- src/utils/filterUsers.js
+++ src/utils/filterUsers.js
@@ -7,6 +7,7 @@ import {
 } from "./selectors";
 
 export default (payload, config, state) => {
+  const { userMapping } = config;
   let filteredData = [];
   let selectedGroup = {};
   if (payload.query === "") {
@@ -19,25 +20,25 @@ export default (payload, config, state) => {
         user.email.indexOf(payload.query) > -1
     );
   }
-  if (payload.query === config.userMapping.all.queryValue) {
+  if (payload.query === userMapping.all.queryValue) {
     filteredData = allSelector(state);
-    selectedGroup = config.userMapping.all;
+    selectedGroup = userMapping.all;
   }
-  if (payload.query === config.userMapping.favorites.queryValue) {
+  if (payload.query === userMapping.favorites.queryValue) {
     filteredData = favoritesSelector(state);
-    selectedGroup = config.userMapping.favorites;
+    selectedGroup = userMapping.favorites;
   }
-  if (payload.query === config.userMapping.administrator.queryValue) {
+  if (payload.query === userMapping.administrator.queryValue) {
     filteredData = administratorSelector(state);
-    selectedGroup = config.userMapping.administrator;
+    selectedGroup = userMapping.administrator;
   }
-  if (payload.query === config.userMapping.nonAdmin.queryValue) {
+  if (payload.query === userMapping.nonAdmin.queryValue) {
     filteredData = nonAdminSelector(state);
-    selectedGroup = config.userMapping.nonAdmin;
+    selectedGroup = userMapping.nonAdmin;
   }
-  if (payload.query === config.userMapping.archived.queryValue) {
+  if (payload.query === userMapping.archived.queryValue) {
     filteredData = archivedSelector(state);
-    selectedGroup = config.userMapping.archived;
+    selectedGroup = userMapping.archived;
   }
   return { filteredData, selectedGroup };
 };
